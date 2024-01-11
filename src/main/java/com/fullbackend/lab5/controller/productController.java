b7a718341eba447bd950e50e49d3fca7d5a29bd9
Morar <morarucristian04@gmail.com>
2024-01-11 17:43:29 +0200

    basics 1.2

--- src/main/java/com/fullbackend/lab5/controller/productController.java
+++ src/main/java/com/fullbackend/lab5/controller/productController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:8081")
 @RestController
 @RequestMapping("/api")
-public class adminController {
+public class productController {
 
     @Autowired
     private ProductService productService;
